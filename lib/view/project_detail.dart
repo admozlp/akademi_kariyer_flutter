afc32ba6f045b3c6c9bfaa5b5233f974ec176396
unknown <efepatnos07@gmail.com>
2023-04-10 05:18:22 +0300

    Porjeler cekildi

--- lib/view/project_detail.dart
+++ lib/view/project_detail.dart
@@ -1,3 +1,4 @@
+import 'package:akademi_kariyer/models/proje.dart';
 import 'package:flutter/material.dart';
 import 'package:akademi_kariyer/models/user.dart';
 import 'package:akademi_kariyer/models/project.dart';
@@ -9,11 +10,8 @@ import '../constants/colors.dart';
 import '../nav/NavigationMenu.dart';
 
 class ProjectDetail extends StatefulWidget {
-  final int projectid;
-  final String  yuzde;
-  final List<Category> categoryname;
-
-  const ProjectDetail({Key? key, required this.projectid,required this.yuzde,required this.categoryname}) : super(key: key);
+  final Proje project;
+  const ProjectDetail(this.project, {Key? key}) : super(key: key);
 
 
   @override
@@ -21,93 +19,13 @@ class ProjectDetail extends StatefulWidget {
 }
 
 class _ProjectDetailState extends State<ProjectDetail> {
-  final List<User> _users = [
-    User(1, 'Elliana Palacios', 'Eşleşme 50%',
-        'https://images.unsplash.com/photo-1504735217152-b768bcab5ebc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=0ec8291c3fd2f774a365c8651210a18b',
-        false),
-    User(2, 'Kayley Dwyer', 'Eşleşme 65%',
-        'https://images.unsplash.com/photo-1503467913725-8484b65b0715?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=cf7f82093012c4789841f570933f88e3',
-        false),
-    User(3, 'Kathleen Mcdonough', 'Eşleşme 45%',
-        'https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b717a6d0469694bbe6400e6bfe45a1da',
-        false),
-    User(4, 'Kathleen Dyer', 'Eşleşme 80%',
-        'https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=ddcb7ec744fc63472f2d9e19362aa387',
-        false),
-    User(5, 'Mikayla Marquez', 'Eşleşme 68%',
-        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(6, 'Kiersten Lange', 'Eşleşme 90%',
-        'https://images.unsplash.com/photo-1542534759-05f6c34a9e63?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(7, 'Carys Metz', 'Eşleşme 72%',
-        'https://images.unsplash.com/photo-1516239482977-b550ba7253f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(8, 'Ignacio Schmidt', 'Eşleşme 92%',
-        'https://images.unsplash.com/photo-1542973748-658653fb3d12?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(9, 'Clyde Lucas', 'Eşleşme 73%',
-        'https://images.unsplash.com/photo-1569443693539-175ea9f007e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(10, 'Mikayla Marquez', 'Eşleşme 99%',
-        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false)
-  ];
-
-  final List<Project> _project=[
-    Project(1, 'ToDo App', 'Bir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir no', 2),
-    Project(2, 'dasdsa App', 'Bir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir no', 5),
-    Project(3, 'ddd App', 'Bir not alma uygulaması', 3),
-    Project(4, 'ToDo dd', 'Bir not alma uygulaması', 7),
-  ];
-
-  final List<Category> _category =[
-    Category(1, 'PHP'),
-    Category(2, 'HTML'),
-    Category(3, 'CSS'),
-    Category(4, 'Angular'),
-    Category(5, 'Pyhon'),
-
-  ];
-
-  final List<CategoryProject> _categoryproject=[
-    CategoryProject(1, 1, 1),
-    CategoryProject(2, 1, 3),
-    CategoryProject(3, 2, 1),
-    CategoryProject(4, 3, 2),
-    CategoryProject(5, 4, 3),
-    CategoryProject(6, 1, 5),
-
-
-  ];
-  final List<UserCategory> _usercategory=[
-    UserCategory(1, 1, 1),
-    UserCategory(2, 1, 3),
-    UserCategory(3, 2, 1),
-    UserCategory(4, 2, 2),
-  ];
-
-  List<User> _foundedUsers = [];
-  List<Project> _foundedProjects = [];
-  //List<Category> _foundedCategory = [];
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      _foundedUsers = _users;
-      _foundedProjects=_project;
-     // _foundedCategory=_category;
-
-
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    var p= _foundedProjects.firstWhere((element) => element.id==widget.projectid);
-    var u=_foundedUsers.firstWhere((element) => element.id==p.userid);
+
+    List<String> kategoriler = widget.project.kategoreiler.split(",");
+    kategoriler.removeAt(kategoriler.length-1);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: bgColor,
@@ -152,22 +70,21 @@ class _ProjectDetailState extends State<ProjectDetail> {
               children: [
                 const SizedBox(height: 14,),
                 Image.network(
-                  u.image,
+                  "https://images.unsplash.com/photo-1504735217152-b768bcab5ebc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=0ec8291c3fd2f774a365c8651210a18b",
                   height: 150,),
-                Text(u.name,style:const TextStyle(color: Colors.deepOrange),),
+                Text(widget.project.olusturan,style:const TextStyle(color: Colors.deepOrange),),
                 const SizedBox(height: 14,),
                 Row(
                   mainAxisAlignment:MainAxisAlignment.spaceBetween,
                   children:[
-                    Text(p.name,
+                    Text(widget.project.baslik,
                       style: const TextStyle(
                         fontSize:23,
                         fontWeight: FontWeight.bold,
                         color: Colors.blue,),
                     ),
-                    Text(widget.yuzde,
-
-                      style: const TextStyle(
+                    const Text("72%",
+                      style: TextStyle(
                         fontSize:18,
                         fontWeight: FontWeight.bold,
                         color: Colors.green,),
@@ -177,7 +94,7 @@ class _ProjectDetailState extends State<ProjectDetail> {
                 Container(
                   alignment: Alignment.topLeft,
                   margin:  const EdgeInsets.all(5.0),
-                  child:Text(p.comment),
+                  child:Text(widget.project.aciklama),
                 ),
                 const SizedBox(height: 8.0,),
                 SingleChildScrollView(
@@ -187,13 +104,13 @@ class _ProjectDetailState extends State<ProjectDetail> {
                         height: 20,
 
                         child: ListView.builder(
-                            itemCount:widget.categoryname.length,
+                            itemCount:kategoriler.length,
                             scrollDirection: Axis.horizontal,
                             itemBuilder: (context,index){
                               return ElevatedButton(
                                 onPressed: () { },
                                 child: Text(
-                                  widget.categoryname[index].name,
+                                  kategoriler[index],
                                   style: const TextStyle(color: Colors.white),
                                 ),
 
--- lib/view/projects.dart
+++ lib/view/projects.dart
@@ -2,12 +2,9 @@ import 'package:akademi_kariyer/components/drawer/custom_drawer.dart';
 import 'package:akademi_kariyer/constants/colors.dart';
 import 'package:akademi_kariyer/nav/NavigationMenu.dart';
 import 'package:akademi_kariyer/view/project_detail.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:akademi_kariyer/models/user.dart';
-import 'package:akademi_kariyer/models/project.dart';
-import 'package:akademi_kariyer/models/Category.dart';
-import 'package:akademi_kariyer/models/ProjectCategory.dart';
-import 'package:akademi_kariyer/models/UserCategory.dart';
+import '../models/proje.dart';
 
 class Projects extends StatefulWidget {
   const Projects({Key? key}) : super(key: key);
@@ -17,142 +14,11 @@ class Projects extends StatefulWidget {
 }
 
 class _ProjectsState extends State<Projects> {
-  final List<User> _users = [
-    User(
-        1,
-        'Elliana Palacios',
-        'Eşleşme 50%',
-        'https://images.unsplash.com/photo-1504735217152-b768bcab5ebc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=0ec8291c3fd2f774a365c8651210a18b',
-        false),
-    User(
-        2,
-        'Kayley Dwyer',
-        'Eşleşme 65%',
-        'https://images.unsplash.com/photo-1503467913725-8484b65b0715?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=cf7f82093012c4789841f570933f88e3',
-        false),
-    User(
-        3,
-        'Kathleen Mcdonough',
-        'Eşleşme 45%',
-        'https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b717a6d0469694bbe6400e6bfe45a1da',
-        false),
-    User(
-        4,
-        'Kathleen Dyer',
-        'Eşleşme 80%',
-        'https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=ddcb7ec744fc63472f2d9e19362aa387',
-        false),
-    User(
-        5,
-        'Mikayla Marquez',
-        'Eşleşme 68%',
-        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(
-        6,
-        'Kiersten Lange',
-        'Eşleşme 90%',
-        'https://images.unsplash.com/photo-1542534759-05f6c34a9e63?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(
-        7,
-        'Carys Metz',
-        'Eşleşme 72%',
-        'https://images.unsplash.com/photo-1516239482977-b550ba7253f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(
-        8,
-        'Ignacio Schmidt',
-        'Eşleşme 92%',
-        'https://images.unsplash.com/photo-1542973748-658653fb3d12?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(
-        9,
-        'Clyde Lucas',
-        'Eşleşme 73%',
-        'https://images.unsplash.com/photo-1569443693539-175ea9f007e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false),
-    User(
-        10,
-        'Mikayla Marquez',
-        'Eşleşme 99%',
-        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-        false)
-  ];
-
-  final List<Project> _project = [
-    Project(
-        1,
-        'ToDo App',
-        'Bir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir no',
-        2),
-    Project(
-        2,
-        'dasdsa App',
-        'Bir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir not alma uygulamasBir no',
-        5),
-    Project(3, 'ddd App', 'Bir not alma uygulaması', 3),
-    Project(4, 'ToDo dd', 'Bir not alma uygulaması', 7),
-  ];
-
-  final List<Category> _category = [
-    Category(1, 'PHP'),
-    Category(2, 'HTML'),
-    Category(3, 'CSS'),
-    Category(4, 'Angular'),
-    Category(5, 'Pyhon'),
-    Category(6, 'New'),
-  ];
-
-  final List<CategoryProject> _categoryproject = [
-    CategoryProject(1, 1, 1),
-    CategoryProject(2, 1, 3),
-    CategoryProject(3, 2, 1),
-    CategoryProject(4, 3, 2),
-    CategoryProject(5, 4, 3),
-    CategoryProject(6, 1, 5),
-    CategoryProject(7, 1, 6),
-    CategoryProject(8, 1, 6),
-  ];
-  final List<UserCategory> _usercategory = [
-    UserCategory(1, 1, 1),
-    UserCategory(2, 1, 3),
-    UserCategory(3, 2, 1),
-    UserCategory(4, 2, 2),
-  ];
-
-  List<User> _foundedUsers = [];
-  List<Project> _foundedProjects = [];
-  List<Category> _foundedCategory = [];
-  List<CategoryProject> _foundedCP = [];
-  List<UserCategory> _foundedUC = [];
-
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      _foundedUsers = _users;
-      _foundedProjects = _project;
-      _foundedCategory = _category;
-      _foundedCP = _categoryproject;
-      _foundedUC = _usercategory;
-    });
-  }
-
-  onSearch(String search) {
-    setState(() {
-      _foundedUsers = _users
-          .where((user) => user.name.toLowerCase().contains(search))
-          .toList();
-      _foundedProjects = _project
-          .where((project) => project.name.toLowerCase().contains(search))
-          .toList();
-      _foundedCategory = _category
-          .where((category) => category.name.toLowerCase().contains(search))
-          .toList();
-      //_foundedCP=_categoryproject.where((categoryproject)=>categoryproject.name.toLowerCase().contains(search)).toList();;
-    });
-  }
+  Stream<List<Proje>> readProject() => FirebaseFirestore.instance
+      .collection("projects")
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Proje.fromJson(doc.data())).toList());
 
   @override
   Widget build(BuildContext context) {
@@ -180,36 +46,38 @@ class _ProjectsState extends State<Projects> {
       drawer: const CustomDrawer(),
       backgroundColor: bgColor,
       bottomNavigationBar: NavigationMenu(size: size),
-      body: ListView.builder(
-          itemCount: _foundedProjects.length,
-          itemBuilder: (context, index) {
-            return userComponent(
-                project: _foundedProjects[index],
-                list: _foundedUsers,
-                usercategory: _foundedUC,
-                categoryproject: _categoryproject,
-                category2: _foundedCategory);
-          }),
+      body: StreamBuilder<List<Proje>>(
+        stream: readProject(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Hata");
+          } else if (snapshot.hasData) {
+            final projects = snapshot.data!;
+            if (projects.isEmpty) {
+              return const Center(
+                child: Text("Henüz Proje Oluşturulmadı"),
+              );
+            }
+            return ListView.builder(
+                itemCount: projects.toList().length,
+                itemBuilder: (context, index) => userComponent(
+                      project: projects.toList()[index],
+                    ));
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 
-  userComponent(
-      {required Project project,
-      required List<User> list,
-      required List<UserCategory> usercategory,
-      required List<CategoryProject> categoryproject,
-      required List<Category> category2}) {
-    int suankiuser = 2;
-    // int userid=project.userid;
-    var newlist = list.firstWhere((id) => id.id == project.userid);
-    var projeusername = newlist.name;
-    var categoryname = projeler(
-        id: project.id, categoryproject: categoryproject, category: category2);
-    var yuzde = range(
-        suankiuserid: suankiuser,
-        projectid: project.id,
-        usercategory: usercategory,
-        categoryproject: categoryproject);
+  userComponent({required Proje project}) {
+    List<String> kategoriler = project.kategoreiler.split(",");
+    kategoriler.removeAt(kategoriler.length-1);
+    
+
 
     return Padding(
       padding: const EdgeInsets.all(15.0),
@@ -237,20 +105,18 @@ class _ProjectsState extends State<Projects> {
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                  //margin:  EdgeInsets.all(8.0),
                   children: [
                     Text(
-                      project.name,
+                      project.baslik,
                       style: const TextStyle(
                         fontSize: 23,
                         fontWeight: FontWeight.bold,
                         color: Colors.blue,
                       ),
                     ),
-                    Text(
-                      yuzde.toString(),
-                      style: const TextStyle(
+                    const Text(
+                      "72%",
+                      style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                         color: Colors.green,
@@ -261,7 +127,7 @@ class _ProjectsState extends State<Projects> {
                 Container(
                   alignment: Alignment.topLeft,
                   margin: const EdgeInsets.all(5.0),
-                  child: Text(project.comment),
+                  child: Text(project.aciklama),
                 ),
                 const SizedBox(
                   height: 8.0,
@@ -272,13 +138,13 @@ class _ProjectsState extends State<Projects> {
                       SizedBox(
                         height: 20,
                         child: ListView.builder(
-                            itemCount: categoryname.length,
+                            itemCount: kategoriler.length,
                             scrollDirection: Axis.horizontal,
                             itemBuilder: (context, index) {
                               return ElevatedButton(
                                 onPressed: () {},
-                                child: Text(
-                                  categoryname[index].name,
+                                child:  Text(
+                                  kategoriler[index],
                                   style: const TextStyle(color: Colors.white),
                                 ),
                               );
@@ -296,12 +162,8 @@ class _ProjectsState extends State<Projects> {
                     ElevatedButton(
                         style: buttonPrimary,
                         onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => ProjectDetail(
-                                    projectid: project.id,
-                                    yuzde: yuzde,
-                                    categoryname: categoryname,
-                                  )));
+                          // go to detay
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>ProjectDetail(project)));
                         },
                         child: const Text(
                           "Daha Fazla",
@@ -316,7 +178,7 @@ class _ProjectsState extends State<Projects> {
                 Container(
                   alignment: Alignment.topLeft,
                   child: Text(
-                    projeusername,
+                    project.olusturan,
                     style: const TextStyle(color: Colors.deepOrange),
                   ),
                 ),
@@ -327,45 +189,6 @@ class _ProjectsState extends State<Projects> {
       ),
     );
   }
-}
-
-List<Category> projeler(
-    {required int id,
-    required List<CategoryProject> categoryproject,
-    required List<Category> category}) {
-  List<Category> categoryname = [];
-  var pc = categoryproject.where((element) => element.projectid == id).toList();
-  for (var p in pc) {
-    categoryname
-        .add(category.firstWhere((element) => element.id == p.categoryid));
-  }
-  return categoryname;
-}
-
-String range(
-    {required int suankiuserid,
-    required int projectid,
-    required List<UserCategory> usercategory,
-    required List<CategoryProject> categoryproject}) {
-  var uc =
-      usercategory.where((element) => element.userid == suankiuserid).toList();
-  var pc = categoryproject
-      .where((element) => element.projectid == projectid)
-      .toList();
-  int count = 0;
-
-  for (var i in pc) {
-    for (var j in uc) {
-      if (j.categoryid == i.categoryid) {
-        count++;
-      }
-    }
-  }
-  var y = ((100 * count) / pc.length);
-  var a = y.ceil();
-  var yuzde = "%$a";
-
-  return yuzde;
 }
 
 final ButtonStyle buttonPrimary = ElevatedButton.styleFrom(
